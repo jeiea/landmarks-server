505bb3d9fc4c4614c750877828f637b2a01a6546
jeiea <solum5013@gmail.com>
2018-08-24 13:34:51 +0900

    Use KlaxonSerializer

--- landmarks-clientkt/src/main/kotlin/kr/ac/kw/coms/landmarks/client/Remote.kt
+++ landmarks-clientkt/src/main/kotlin/kr/ac/kw/coms/landmarks/client/Remote.kt
@@ -2,35 +2,47 @@ package kr.ac.kw.coms.landmarks.client
 
 import com.beust.klaxon.JsonBase
 import com.beust.klaxon.JsonObject
+import com.beust.klaxon.KlaxonJson
 import com.beust.klaxon.Parser
 import com.beust.klaxon.internal.firstNotNullResult
-import com.beust.klaxon.json
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.features.cookies.AcceptAllCookiesStorage
 import io.ktor.client.features.cookies.HttpCookies
+import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.*
 import io.ktor.http.*
-import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.sendBlocking
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.io.jvm.javaio.toOutputStream
 import java.io.File
 import java.util.*
 import kotlin.math.max
 
-class Remote(val http: HttpClient, val basePath: String) {
+class Remote(base: HttpClient, val basePath: String) {
+
+  val http: HttpClient
+  val nominatimReqLimit = Channel<Long>(1)
 
   companion object {
     const val herokuUri = "https://landmarks-coms.herokuapp.com"
+    private const val chromeAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.59 Safari/537.36"
   }
 
-  constructor() : this(HttpClient(Android.create()) {
-    install(HttpCookies) {
-      storage = AcceptAllCookiesStorage()
+  init {
+    nominatimReqLimit.sendBlocking(0)
+    http = base.config {
+      install(HttpCookies) {
+        storage = AcceptAllCookiesStorage()
+      }
+      install(JsonFeature) {
+        serializer = KlaxonSerializer()
+      }
     }
-  }, herokuUri)
+  }
 
-  val lastHostReqTime = hashMapOf<String, Long>()
-  val chromeAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.59 Safari/537.36"
+  constructor() : this(HttpClient(Android.create()), herokuUri)
 
   private fun HttpRequestBuilder.userAgent() {
     header("User-Agent", "landmarks-client")
@@ -38,13 +50,18 @@ class Remote(val http: HttpClient, val basePath: String) {
 
   private fun HttpRequestBuilder.json(json: JsonBase) {
     contentType(ContentType.Application.Json)
-    body = json.toJsonString()
+    body = json
   }
 
-  @Synchronized
-  suspend fun reverseGeocode(latitude: Double, longitude: Double): Pair<String?, String?>? {
-    val last: Long = lastHostReqTime["nominatim.openstreetmap.org"] ?: 0
+  suspend fun suspendForNominatimReqPerSecLimit() {
+    val last: Long = nominatimReqLimit.receive()
+    nominatimReqLimit.offer(Date().time)
     delay(max(0, last + 1000 - Date().time))
+  }
+
+  suspend fun reverseGeocode(latitude: Double, longitude: Double): Pair<String?, String?>? {
+    suspendForNominatimReqPerSecLimit()
+
     val json: String = http.get("https://nominatim.openstreetmap.org/reverse") {
       parameter("format", "json")
       parameter("lat", latitude.toString())
@@ -64,19 +81,17 @@ class Remote(val http: HttpClient, val basePath: String) {
   }
 
   suspend fun checkAlive(): Boolean {
-    val resp: String = http.get(basePath)
+    val resp: String = http.get("$basePath/")
     return resp.contains("Hello")
   }
 
   suspend fun register(ident: String, pass: String, email: String, nick: String) {
-    val regFields: JsonObject = json {
-      obj(
-        "login" to ident,
-        "password" to pass,
-        "email" to email,
-        "nick" to nick
-      )
-    }
+    val regFields: JsonObject = KlaxonJson().obj(
+      "login" to ident,
+      "password" to pass,
+      "email" to email,
+      "nick" to nick
+    )
     val resp: String = http.post("$basePath/auth/register") {
       userAgent()
       json(regFields)
@@ -85,7 +100,10 @@ class Remote(val http: HttpClient, val basePath: String) {
   }
 
   suspend fun login(ident: String, pass: String) {
-    val param: JsonObject = json { obj("login" to ident, "password" to pass) }
+    val param: JsonObject = KlaxonJson().obj(
+      "login" to ident,
+      "password" to pass
+    )
     val resp: String = http.post("$basePath/auth/login") {
       userAgent()
       json(param)
@@ -107,5 +125,20 @@ class Remote(val http: HttpClient, val basePath: String) {
       }
     }
     throwIfFailure(resp)
+  }
+
+  data class PictureRep(
+    val id: Int,
+    val owner: Int,
+    val address: String,
+    val lat: Float,
+    val lon: Float,
+    var file: ByteArray?
+  )
+
+  suspend fun getRandomProblem(): PictureRep {
+    val pic: PictureRep = http.get("$basePath/problem/random")
+    pic.file = http.get("$basePath/picture/${pic.id}")
+    return pic
   }
 }
